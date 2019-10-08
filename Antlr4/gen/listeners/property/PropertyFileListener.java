93477899a0460b88b01cfb3092acd0dde522626f
Zhang Lei <942471854@qq.com>
2019-10-09 00:53:48 +0800

    commit-调整文件结构，学习至7.1

---
+++ Antlr4/gen/listeners/property/PropertyFileListener.java
@@ -0,0 +1,30 @@
+// Generated from E:/Coding/Java/Program/Antlr4/src/listeners/property\PropertyFile.g4 by ANTLR 4.7.2
+package listeners.property;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link PropertyFileParser}.
+ */
+public interface PropertyFileListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link PropertyFileParser#file}.
+	 * @param ctx the parse tree
+	 */
+	void enterFile(PropertyFileParser.FileContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PropertyFileParser#file}.
+	 * @param ctx the parse tree
+	 */
+	void exitFile(PropertyFileParser.FileContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PropertyFileParser#prop}.
+	 * @param ctx the parse tree
+	 */
+	void enterProp(PropertyFileParser.PropContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PropertyFileParser#prop}.
+	 * @param ctx the parse tree
+	 */
+	void exitProp(PropertyFileParser.PropContext ctx);
+}
