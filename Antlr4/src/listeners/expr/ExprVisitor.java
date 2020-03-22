76bba1e9b03e0ec1778714c6d12d44043e152dee
Zhang Lei <942471854@qq.com>
2020-03-22 15:44:25 +0800

    commit

---
+++ Antlr4/src/listeners/expr/ExprVisitor.java
@@ -0,0 +1,25 @@
+// Generated from E:/Coding/Java/Program/Antlr4/src/listeners/expr\Expr.g4 by ANTLR 4.7.2
+package listeners.expr;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link ExprParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface ExprVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link ExprParser#s}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitS(ExprParser.SContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ExprParser#e}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitE(ExprParser.EContext ctx);
+}
